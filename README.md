317277027c86df6ced372cf5814062243333c657
Kristina Volobueva <musickristy@yandex.ru>
2024-08-26 12:31:45 +0300

    add readme

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
 # Проектная работа Mesto
-
+https://github.com/Volobueva-Kristina/mesto-project-ff.git
--- scripts/index.js
+++ scripts/index.js
@@ -1,36 +1,39 @@
 // @todo: Темплейт карточки
-const cardsTemp = document.querySelector('#card-template');
+const cardsTemp = document.querySelector("#card-template");
 
 // @todo: DOM узлы
-const placesContainer = document.querySelector('.places__list');
-const profile = document.querySelector('.profile');
-const addButton = profile.querySelector('.profile__add-button');
-const resetButton = document.querySelector('.card__delete-button');
+const placesContainer = document.querySelector(".places__list");
+const profile = document.querySelector(".profile");
+const addButton = profile.querySelector(".profile__add-button");
+const resetButton = document.querySelector(".card__delete-button");
 
 // @todo: Функция создания карточки
 function addCards(placeValue, pictureValue) {
-    const cardsTemplate = cardsTemp.content;
-    const cardElement = cardsTemplate.querySelector('.card').cloneNode(true);
-  
-    cardElement.querySelector('.card__image').src = pictureValue;
-    cardElement.querySelector('.card__delete-button').addEventListener('click', function (evt) {
-        const eventTarget = evt.target.closest('.card');
-        removeCard(eventTarget);
-    });
-    cardElement.querySelector('.card__title').textContent = placeValue;
-    cardElement.querySelector('.card__like-button').addEventListener("click", function (evt) {
-        evt.target.classList.toggle("card__like-button_is-active");
-    });
-    placesContainer.append(cardElement);
-  };
+  const cardsTemplate = cardsTemp.content;
+  const cardElement = cardsTemplate.querySelector(".card").cloneNode(true);
 
+  cardElement.querySelector(".card__image").src = pictureValue;
+  cardElement
+    .querySelector(".card__delete-button")
+    .addEventListener("click", function (evt) {
+      const eventTarget = evt.target.closest(".card");
+      removeCard(eventTarget);
+    });
+  cardElement.querySelector(".card__title").textContent = placeValue;
+  cardElement
+    .querySelector(".card__like-button")
+    .addEventListener("click", function (evt) {
+      evt.target.classList.toggle("card__like-button_is-active");
+    });
+  placesContainer.append(cardElement);
+}
 
 // @todo: Функция удаления карточки
 function removeCard(removeCardItem) {
-    removeCardItem.remove();
-  }
+  removeCardItem.remove();
+}
 
 // @todo: Вывести карточки на страницу
 initialCards.forEach(function (element) {
-    addCards(element.name, element.link);
+  addCards(element.name, element.link);
 });
