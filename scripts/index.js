f7599061d5a8791983edc4cabb082d09207a70b7
Kristina Volobueva <musickristy@yandex.ru>
2024-08-27 10:56:37 +0300

    fixed card template

--- scripts/index.js
+++ scripts/index.js
@@ -1,39 +1,43 @@
 // @todo: Темплейт карточки
-const cardsTemp = document.querySelector("#card-template");
+const cardsTemplate = document.querySelector("#card-template");
 
 // @todo: DOM узлы
 const placesContainer = document.querySelector(".places__list");
 const profile = document.querySelector(".profile");
 const addButton = profile.querySelector(".profile__add-button");
-const resetButton = document.querySelector(".card__delete-button");
 
 // @todo: Функция создания карточки
-function addCards(placeValue, pictureValue) {
-  const cardsTemplate = cardsTemp.content;
-  const cardElement = cardsTemplate.querySelector(".card").cloneNode(true);
+function createCard(CardDate, removeCard) {
+  const cardElement = cardsTemplate.content
+    .querySelector(".card")
+    .cloneNode(true);
 
-  cardElement.querySelector(".card__image").src = pictureValue;
-  cardElement
-    .querySelector(".card__delete-button")
-    .addEventListener("click", function (evt) {
-      const eventTarget = evt.target.closest(".card");
-      removeCard(eventTarget);
-    });
-  cardElement.querySelector(".card__title").textContent = placeValue;
-  cardElement
-    .querySelector(".card__like-button")
-    .addEventListener("click", function (evt) {
-      evt.target.classList.toggle("card__like-button_is-active");
-    });
-  placesContainer.append(cardElement);
+  const cardElementTitle = cardElement.querySelector(".card__title");
+  const cardElementImage = cardElement.querySelector(".card__image");
+  const cardElementDelButton = cardElement.querySelector(
+    ".card__delete-button"
+  );
+
+  cardElementTitle.textContent = CardDate.name;
+  cardElementImage.src = CardDate.link;
+  cardElementImage.alt = CardDate.name;
+
+  cardElementDelButton.addEventListener("click", removeCard);
+
+  return cardElement;
 }
 
 // @todo: Функция удаления карточки
-function removeCard(removeCardItem) {
-  removeCardItem.remove();
+function removeCard(evt) {
+  const card = evt.target.closest(".card");
+  card.remove();
+}
+
+function renderCard(cardElement) {
+  placesContainer.append(cardElement);
 }
 
 // @todo: Вывести карточки на страницу
-initialCards.forEach(function (element) {
-  addCards(element.name, element.link);
+initialCards.forEach((element) => {
+  renderCard(createCard(element, removeCard));
 });
